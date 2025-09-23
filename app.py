c932b06c15505831b3ea02a46971b11f10b2c7ed
Luke Henderson <personal@lukeh.us>
2025-09-23 02:24:33 -0700

    tailwind refactor

--- app.py
+++ app.py
@@ -1,15 +1,67 @@
-from website import start
-from dotenv import load_dotenv
-import os
+from flask import Flask, render_template, redirect
+app = Flask(__name__)
 
-load_dotenv()
+@app.route("/")
+def home():
+    return(
+        render_template(
+            "index.html"
+        )
+    )
 
-SERVER_NAME = os.getenv("SERVER_NAME")
-SERVER_PORT = os.getenv("SERVER_PORT")
+@app.route("/yt")
+@app.route("/youtube")
+def youtube():
+    return(
+        redirect(
+            "https://www.youtube.com/@lyciberi"
+        )
+    )
 
-app = start()
+@app.route("/t")
+@app.route("/x")
+@app.route("/twt")
+@app.route("/twitter")
+def twitter():
+    return(
+        redirect(
+            "https://twitter.com/lyciberi"
+        )
+    )
+
+@app.route("/ttv")
+@app.route("/twitch")
+def twitch():
+    return(
+        redirect(
+            "https://www.twitch.tv/lyciberi"
+        )
+    )
+
+@app.route("/discord")
+def discord():
+    return(
+        redirect(
+            "https://discord.com/invite/Ywh4xN6YFX"
+        )
+    )
+
+@app.route("/bsky")
+@app.route("/bluesky")
+def bluesky():
+    return(
+        redirect(
+            "https://bsky.app/profile/lyciberi.bsky.social"
+        )
+    )
+
+@app.route("/tiktok")
+def tiktok():
+    return(
+        redirect(
+            "https://www.tiktok.com/@lyciberi"
+        )
+    )
 
 if(__name__ == "__main__"):
-    app.config["SERVER_NAME"] = f"{SERVER_NAME}:{SERVER_PORT}"
-    app.config["SESSION_COOKIE_DOMAIN"] = f".{SERVER_NAME}"
     app.run(host="0.0.0.0", debug=True)
